457d5b08d308f8a38fd33b126788157d84be46dc
bill K7WXW <91234299+K7WXW@users.noreply.github.com>
2021-12-04 14:53:57 -0800

    Update generate.py
    # K7WXW V0.1 - build bare codeplug with just the pnwdigital list
    # and an updated include list. comment out seattledmr, default_k7abd,
    # scan list.
    # uses updated generation.py V0.1

--- codeplug/default-tyt-md380/generate.py
+++ codeplug/default-tyt-md380/generate.py
@@ -3,6 +3,10 @@
 # This script generates the same codeplug as generate.sh
 # by running dzcb via the python API
 
+# K7WXW V0.1 - build bare codeplug with just the pnwdigital list
+# and an updated include list. comment out seattledmr, default_k7abd,
+# scan list.
+
 from pathlib import Path
 import os
 
@@ -13,9 +17,9 @@ output = Path(os.environ.get("OUTPUT") or (cp_dir / ".." / ".." / "OUTPUT"))
 
 CodeplugRecipe(
     source_pnwdigital=True,
-    source_seattledmr=True,
-    source_default_k7abd=True,
-    scanlists_json=cp_dir / "scanlists.json",
+#    source_seattledmr=True,
+#    source_default_k7abd=True,
+#    scanlists_json=cp_dir / "scanlists.json",
     exclude=cp_dir / "exclude.csv",
     output_farnsworth=cp_dir.glob("md3?0-?hf.json"),
 ).generate(output / cp_dir.name)
